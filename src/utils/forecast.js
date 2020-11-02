ce30991dfa1578ca4adb89f90db3f8f2e9d671fe
Ashish <65097624+as9704487@users.noreply.github.com>
2020-11-02 11:16:21 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=f1c5a465f83a507227b3b80e03dedb5d&units=metric";
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather service", undefined);
+
+        } else if (body.message) {
+            callback("Unable to find location", undefined)
+
+        }
+        else {
+            callback(undefined, "Today " + body.weather[0].description + ". It is currently " + body.main.temp + " degree out there.")
+
+        }
+    })
+
+}
+
+module.exports = forecast
